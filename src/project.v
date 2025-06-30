443a3db1f076c40eb5e4e294f890bb0c64b85efe
Umar316798 <umar.ahamed@sjsu.edu>
2025-06-29 19:58:12 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,14 +1,17 @@
+/*
+ * Tiny Logic-Based Inference Classifier
+ * Author: Umar
+ * SPDX-License-Identifier: Apache-2.0
  */
 
-default_nettype none
+`default_nettype none
 
 module tt_um_umar316798 (
     input  wire [2:0] ui_in,   // 3 dedicated input bits: motion, door, window
     output wire       uo_out   // 1 dedicated output bit: alarm
 );
 
-  // Simple smart alarm logic:
-  // If (motion AND door) OR window → alarm ON
+  // Smart alarm logic:
   assign uo_out = (ui_in[0] & ui_in[1]) | ui_in[2];
 
 endmodule
