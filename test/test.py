6719f048ae8d1dd107fc82cb841eabe25f5954ff
Umar316798 <umar.ahamed@sjsu.edu>
2025-07-02 12:10:45 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,1 +1,10 @@
+import cocotb
+from cocotb.triggers import Timer
+
+@cocotb.test()
+async def simple_test(dut):
+    """A simple example test"""
+    dut._log.info("Running simple test")
+    await Timer(10, units='ns')
+    assert True
 
