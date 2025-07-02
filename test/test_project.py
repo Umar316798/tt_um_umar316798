327ea0c84923e64dddf23502bf45345e331bc04a
Umar316798 <umar.ahamed@sjsu.edu>
2025-07-02 12:27:18 -0700

    Update test_project.py

--- test/test_project.py
+++ test/test_project.py
@@ -2,17 +2,14 @@ import cocotb
 from cocotb.triggers import Timer
 
 @cocotb.test()
-async def test_project(dut):
-    dut.uio_in.value = 0
+async def my_simple_test(dut):
+    dut._log.info("Starting test")
     dut.clk.value = 0
     dut.ena.value = 1
     dut.rst_n.value = 1
+    dut.uio_in.value = 0
 
-    dut._log.info("Time | motion door window | alarm")
-
-    for i in range(8):
+    for i in range(4):
         dut.ui_in.value = i
         await Timer(10, units='ns')
-        dut._log.info(f"{i}: {dut.ui_in.value.bin} -> {dut.uo_out.value.bin}")
-
-    dut._log.info("Test done.")
+        dut._log.info(f"Input: {dut.ui_in.value.bin} Output: {dut.uo_out.value.bin}")
